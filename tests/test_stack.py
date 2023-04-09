5e9ba5483437033ab7ca9f78a236ebe180a36941
Ivan Parkhomenko <ippeng54@gmail.com>
2023-04-09 15:36:38 +0300

    execute homework1

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,27 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Node, Stack
+
+
+# @pytest.fixture()
+# def item_pc():
+#     return Item("ПК", 15000, 2)
+class TestStack(unittest.TestCase):
+    node = Node(5, 6)
+    stack = Stack()
+    stack.push(10)
+
+    # Внутри пишем нужное количество методов (функций)
+    def test_stack_node_init(self):
+        # Пишем нужное количество проверок
+        self.assertEqual(TestStack.node.data, 5)
+        self.assertEqual(TestStack.node.next_node, 6)
+
+    def test_stack_stack_init(self):
+        # Пишем нужное количество проверок
+        self.assertIsInstance(TestStack.stack.top, Node)
+        self.assertIsInstance(TestStack.stack, Stack)
+        self.assertEqual(TestStack.stack.top.data, 10)
+        self.assertEqual(TestStack.stack.top.next_node, None)
+
+
